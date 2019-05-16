206d9d076d1a8c9920fd50323d5a67350988f146
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-16 12:22:04 +0200

    Update test_dk.py
    c du pipo c test jvois pas ou en faire a par tout refaire...

--- dk/test_dk.py
+++ dk/test_dk.py
@@ -1,50 +1,141 @@
+from classes import *
+
 import dklabyrinthe as script
 import classes as script1
 
 
 
-class TestClass(object):
-    
+                    
+from database import *
+
+class TestClass(object):    
     def test_trait_list(self):
         parametre = [(1, "['droite', 'droite', 'bas', 'droite', 'droite']")]
-
         sortie = ['droite', 'droite', 'bas', 'droite', 'droite']
+        
         assert script.trait_list(parametre) == sortie
 
 
     def test_fichier(self):
-
         sortie = 'requete2.py'
+    
         assert script.fichier() == sortie
 
+    def test_choice_level(self):
+        'We verify the min move from database'
+        out = 6
+
+        assert script1.help_method.traitement_database(self) == out
+
+
+    def test_s_point_1(self):
+        'We verify the min move from database\
+        if dk moving on s from the bottom'
+        
+        self.y = 1
+        parametre = self.y
+        out = (6, 's')
+
+        assert script1.help_method.s_point_1(self, parametre) == out
+
+    def test_s_point_1(self):
+        'We verify the min move from database if\
+        dk moving on s from the right'
+        
+        self.x = 1
+        parametre = self.x
+        out = (6, 's')
+
+        assert script1.help_method.s_point_1(self, parametre) == out
+
+    def test_s_point_2(self):
+        'We verify the min move from database\
+        if dk moving on s from the left'
+        
+        self.x = 1
+        parametre = self.x
+        out = (6, 's')
+
+        assert script1.help_method.s_point_1(self, parametre) == out
 
 
 
+    def test_s_point_2(self):
+        'We verify the min move from database if\
+        dk moving on s from the top'
+        
+        self.y = 1
+        parametre = self.y
+        out = (6, 's')
+
+        assert script1.help_method.s_point_1(self, parametre) == out
 
 
+    def test_right(self):
+        'we check that if dk is not on arrival he can move to right'
+
+      
+        self.case_x = 30
+        self.case_y = 30
+        self.x = 0
+        self.y = 0
+        
+        out = None
+
+        assert script1.Perso.adroite(self) == out
 
 
+    def test_bot(self):
+        'we check that if dk is not on arrival he can move to bottom'
+        
+      
+        self.case_x = 30
+        self.case_y = 30
+        self.x = 0
+        self.y = 0
+        
+        out = None
+
+        assert script1.Perso.abas(self) == out
 
 
+    def test_left(self):
+        'we check that if dk is not on arrival he can move to left'
+        
+      
+        self.case_x = 0
+        self.case_y = 30
+        self.x = 0
+        self.y = 0
+        
+        out = None
+
+        assert script1.Perso.agauche(self) == out
+
+    def test_top(self):
+        'we check that if dk is not on arrival he can move to top'
+        
+        self.case_x = 0
+        self.case_y = -30
+        self.x = 0
+        self.y = 0
+        
+        out = None
+
+        assert script1.Perso.ahaut(self) == out
 
 
+    def test_top(self):
+        'we check that if dk is not on arrival he can move to top'
 
+        self.case_x = 0
+        self.case_y = 60
+        self.x = 0
+        self.y = 0
+        
+        out = None
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        assert script1.Perso.agauche(self) == out
 
 
 
