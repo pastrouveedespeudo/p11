050a6c94ae5b40984c1a4070efae34f7ff842253
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-16 14:01:30 +0200

    Update classes.py

--- dk/classes.py
+++ dk/classes.py
@@ -1,4 +1,4 @@
-"""Classes du jeu de Labyrinthe Donkey Kong"""
+"""Classes for the game dk"""
 
 import pygame
 from pygame.locals import * 
@@ -8,88 +8,94 @@ from database import *
 
 
 class Niveau:
-    """Classe permettant de créer un niveau"""
-    def __init__(self, fichier):
-        self.fichier = fichier
+    """Classe who create level"""
+    
+    def __init__(self, file):
+        self.file = file
         self.structure = 0
         
     def choice_level(self):
+        'here user can choice his level'
         
-        continuer_jeu = 1
-        continuer_accueil = 1
+        continue_game = 1
+        continuer_home = 1
 
-        while continuer_accueil:
+        while continuer_home:
             
             pygame.time.Clock().tick(30)
             
             for event in pygame.event.get():
             
                 if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
-                    continuer_accueil = 0
-                    continuer_jeu = 0
+                    continuer_home = 0
+                    continue_game = 0
                     continuer = 0
-                    choix = 0
+                    choice = 0
                         
                 elif event.type == KEYDOWN:				
               
                     if event.key == K_F1:
-                        continuer_accueil = 0
-                        choix = 'n1'		
+                        continuer_home = 0
+                        choice = 'n1'		
                         return 'n1'
                     
                     elif event.key == K_F2:
-                        continuer_accueil = 0
-                        choix = 'n2'
+                        continuer_home = 0
+                        choice = 'n2'
                         return 'n2'
 
 
 
-    def generer(self):
-
-        with open(self.fichier, "r") as fichier:
+    def generate(self):
+        'from n1 we generating wall and free way'
+        
+        with open(self.file, "r") as file:
             structure_niveau = []
             
-            for ligne in fichier:
-                ligne_niveau = []
+            for line in file:
+                line_niveau = []
       
-                for sprite in ligne:
+                for sprite in line:
                     if sprite != '\n':
-                        ligne_niveau.append(sprite)
+                        line_niveau.append(sprite)
 
-                structure_niveau.append(ligne_niveau)
+                structure_niveau.append(line_niveau)
             self.structure = structure_niveau
 
     
-    def afficher(self, fenetre):
-
-        mur = pygame.image.load(image_mur).convert()
-        depart = pygame.image.load(image_depart).convert()
-        arrivee = pygame.image.load(image_arrivee).convert_alpha()
+    def display(self, window):
+        'we displaying wall, window ...'
         
-        num_ligne = 0
-        for ligne in self.structure:
+        mur = pygame.image.load(image_wall).convert()
+        depart = pygame.image.load(image_departure).convert()
+        arrivee = pygame.image.load(image_arrival).convert_alpha()
+        
+        num_line = 0
+        for line in self.structure:
             num_case = 0
             
-            for sprite in ligne:
-                x = num_case * taille_sprite
-                y = num_ligne * taille_sprite
+            for sprite in line:
+                x = num_case * size_sprite
+                y = num_line * size_sprite
                 
                 if sprite == 'm' or sprite == 'M':		   
-                    fenetre.blit(mur, (x,y))
+                    window.blit(mur, (x,y))
                 elif sprite == 'd':		   
-                    fenetre.blit(depart, (x,y))
+                    window.blit(depart, (x,y))
                 elif sprite == 's':		   
-                    fenetre.blit(arrivee, (x,y))
+                    window.blit(arrivee, (x,y))
                 num_case += 1
-            num_ligne += 1
+            num_line += 1
                     
                     
 LISTE = []
 
 class help_method:
+    'methods called by program'
     
-
-    def traitement_database(self):
+    def traitement_dattodowne(self):
+        'we called data from database here it\'s the minimum of moves'
+        
         visuel = visualisation_table.visualisation(self)
         visuel = str(visuel)
         visuel = visuel.split()
@@ -97,24 +103,31 @@ class help_method:
         return len(visuel)
 
     def s_point_1(self, case):
+        'the case is the arrival ? for right and\
+        down moves, if yes we return minimum of moves'
+        
         self.case = case
         
-        number_minimal_movement = help_method.traitement_database(self)
+        number_minimal_movement = help_method.traitement_dattodowne(self)
         self.case += 1
         return number_minimal_movement, 's'   
 
 
     def s_point_2(self, case):
+        'the case is the arrival ? for left and\
+        top moves, if yes we return minimum of moves'
+        
         self.case = case
         
         self.case -= 1
-        number_minimal_movement = help_method.traitement_database(self)
+        number_minimal_movement = help_method.traitement_dattodowne(self)
         return number_minimal_movement, 's'
     
 
           
 class Perso:
-    """Classe permettant de créer un personnage"""
+    """Class who create perso"""
+    
     def __init__(self, droite, gauche, haut, bas, niveau):
         
         self.droite = pygame.image.load(droite).convert_alpha()
@@ -132,7 +145,10 @@ class Perso:
         self.niveau = niveau
     
 
-    def adroite(self):
+    def torigth(self):
+        'here we can move to the right if it\s not a wall, if it\'s arrival we\
+        return s_point function'
+        
         if self.case_x < (nombre_sprite_cote - 1):
             if self.niveau.structure[self.case_y][self.case_x+1] == 's':
 
@@ -142,11 +158,13 @@ class Perso:
             if self.niveau.structure[self.case_y][self.case_x+1] != 'M':
                 self.case_x += 1
                 LISTE.append((self.x, self.y))
-                self.x = self.case_x * taille_sprite
+                self.x = self.case_x * size_sprite
 
 
-    def agauche(self):
-         
+    def toleft(self):
+        'here we can move to the left if it\s not a wall, if it\'s arrival we\
+        return s_point function'
+        
         if self.case_x > 0:
              if self.niveau.structure[self.case_y][self.case_x-1] == 's':
 
@@ -156,10 +174,13 @@ class Perso:
              if self.niveau.structure[self.case_y][self.case_x-1] != 'M':
                 self.case_x -= 1
                 LISTE.append((self.x, self.y))
-                self.x = self.case_x * taille_sprite
+                self.x = self.case_x * size_sprite
                 
             
-    def abas(self):
+    def todown(self):
+        'here we can move to the bottom if it\s not a wall, if it\'s arrival we\
+        return s_point function'
+        
         if self.case_y < (nombre_sprite_cote - 1):
             if self.niveau.structure[self.case_y + 1][self.case_x] == 's':
 
@@ -169,11 +190,13 @@ class Perso:
             if self.niveau.structure[self.case_y+1][self.case_x] != 'M':
                 self.case_y += 1
                 LISTE.append((self.x, self.y))
-                self.y = self.case_y * taille_sprite
+                self.y = self.case_y * size_sprite
 
 
-    def ahaut(self):
-
+    def totop(self):
+        'here we can move to the top if it\s not a wall, if it\'s arrival we\
+        return s_point function'
+        
         if self.case_y > 0:
             if self.niveau.structure[self.case_y-1][self.case_x] == 's':
 
@@ -183,11 +206,12 @@ class Perso:
             if self.niveau.structure[self.case_y-1][self.case_x] != 'M':
                 self.case_y -= 1
                 LISTE.append((self.x, self.y))
-                self.y = self.case_y * taille_sprite
+                self.y = self.case_y * size_sprite
 
 
-    def deplacer(self, direction):
-        """Methode permettant de déplacer le personnage"""
+    def deplacement(self, direction):
+        """Methode who deplace the perso"""
+        
         LISTE = []
 
         filee = []
@@ -200,19 +224,19 @@ class Perso:
             pass
 
         if direction == 'droite':
-            sauve = Perso.adroite(self)
+            sauve = Perso.torigth(self)
             return sauve
         
         elif direction == 'gauche':
-            sauve = Perso.agauche(self)
+            sauve = Perso.toleft(self)
             return sauve
         
         elif direction == 'haut':      
-            sauve = Perso.ahaut(self)
+            sauve = Perso.totop(self)
             return sauve
         
         elif direction == 'bas':
-            sauve = Perso.abas(self)
+            sauve = Perso.todown(self)
             return sauve
         
 
